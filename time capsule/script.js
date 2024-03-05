6f8bbe48e1a4d5a6270a0a8b75c4c6354420adcc
sarashaikhh <sara.shaikh.nc@gmail.com>
2024-03-05 10:59:17 -0500

    hi

---
+++ time capsule/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mask = document.querySelector('.mask');
+
+    // Add event listener to catch mouse movements on the document
+    document.addEventListener('mousemove', (event) => {
+        // Calculate mouse position in percentages
+        let x = event.clientX / window.innerWidth * 100;
+        let y = event.clientY / window.innerHeight * 100;
+
+        // Update the custom property values on the body
+        mask.style.setProperty('--mouse-x', x + '%');
+        mask.style.setProperty('--mouse-y', y + '%');
+    });
+});
